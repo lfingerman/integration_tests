30a013435643acbaffa7fac69f38af7e1ed346a8
Leonard Fingerman <leonard@is-mbp-leonard.in.ionicsecurity.com>
2018-03-05 16:00:20 -0500

    Updated integration tests

--- spec/service_integration_spec.rb
+++ spec/service_integration_spec.rb
@@ -4,6 +4,13 @@ describe 'Record Importer Webservice' do
 
     let(:vin) {VinGenerator.generate_vin}
     let(:record) { ConsumerRecord.find_by(id: @record_id) }
+    let(:updated_record) {}
+    let(:update_payload_xml) { '<<~XML
+        <record>
+          <id>#{record_id}</id>
+          <reason>updated</reason>
+        </record>
+      XML'}
 
     before(:each) do
       @record_id = RecordImporterServiceHelper.create_record(vin: VinGenerator.generate_vin)
@@ -11,20 +18,12 @@ describe 'Record Importer Webservice' do
 
     it 'creates a record' do
       expect(record.status).to eq(Status::Imported)
-
     end
 
     it 'updates a record' do
-
-      update_payload_xml = <<~XML
-        <record>
-          <id>#{record_id}</id>
-          <reason>test</reason>
-        </record>
-      XML
-
-      RecordImporterServiceHelper.create_record(record_id: record.id, uodate_payload:  )
-
+      RecordImporterServiceHelper.update_record(record_id: record.id, update_payload: update_payload_xml)
+      updated_record = ConsumerRecord.find_by(id: record.id)
+      expect(updated_record.reason).to eq('updated')
     end
 
     after(:each) do
